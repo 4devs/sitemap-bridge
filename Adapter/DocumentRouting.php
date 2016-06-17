52c9784f4fda2e18e79d78956cd53db67e591b75
andrey1s <andrey@4devs.pro>
2016-06-17 15:50:38 +0300

    add default priority and freq document routing

--- Adapter/DocumentRouting.php
+++ Adapter/DocumentRouting.php
@@ -3,6 +3,7 @@
 namespace FDevs\Bridge\Sitemap\Adapter;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
+use FDevs\Sitemap\Model\ChangeFrequency;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
@@ -19,6 +20,14 @@ class DocumentRouting extends AbstractRouting
     private $securityContext;
 
     /**
+     * @var array
+     */
+    private $default = [
+        'priority' => 0.7,
+        'changefreq' => ChangeFrequency::WEEKLY,
+    ];
+
+    /**
      * {@inheritdoc}
      */
     public function __construct(UrlGeneratorInterface $urlGenerator, $eventDispatcher, DocumentRepository $repo, AuthorizationCheckerInterface $securityContext = null)
@@ -26,6 +35,19 @@ class DocumentRouting extends AbstractRouting
         parent::__construct($urlGenerator, $eventDispatcher);
         $this->securityContext = $securityContext;
         $this->repo = $repo;
+    }
+
+    /**
+     * @param string     $name
+     * @param mixed|null $value null if need delete default value
+     *
+     * @return  $this
+     */
+    public function setDefault($name, $value)
+    {
+        $this->default[$name] = $value;
+
+        return $this;
     }
 
     /**
@@ -51,11 +73,17 @@ class DocumentRouting extends AbstractRouting
     {
         $url = null;
         $name = $item->getName();
+        foreach ($this->default as $property => $value) {
+            if ($value !== null && !$item->hasDefault($property)) {
+                $item->setDefault($property, $value);
+            }
+        }
         if ($this->securityContext && method_exists($item, 'getContent')) {
             try {
                 $content = $item->getContent();
                 if ($content && $this->securityContext->isGranted('VIEW_ANONYMOUS', $content)) {
                     $item->setDefault('priority', 0.7);
+                    $item->setDefault('changefreq', ChangeFrequency::WEEKLY);
                     $url = parent::createUrl($name, $params, $item);
                 }
             } catch (\Exception $e) {
